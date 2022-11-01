272800fd6b5ce30b9bc056f837bef70896f5d831
Onimisi Owolewa <Honymissyowolewa1@gmail.com>
2022-10-31 19:55:58 -0700

    Pushing tp github

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './pricing.html', './contact.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
